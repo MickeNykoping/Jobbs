8efc02e286748038a379fa8a7e47516ffbff2b98
MickeNykoping <mikael.rova@outlook.com>
2026-08-12 20:10:10 +0200

    Update fetch_jobs.js

--- fetch_jobs.js
+++ fetch_jobs.js
@@ -1,1 +1,15 @@
+// fetch_jobs.js
+const fs = require("fs");
+const fetch = require("node-fetch");
+
+async function updateJobs() {
+    const url = "https://jobsearch.api.jobtechdev.se/search?q=IT&limit=200";
+    const res = await fetch(url);
+    const data = await res.json();
+
+    fs.writeFileSync("jobs.json", JSON.stringify(data.hits, null, 2));
+    console.log("Jobb uppdaterade:", new Date());
+}
+
+updateJobs();
 
